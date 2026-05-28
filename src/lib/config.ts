eebb45a4ec13691aa5e1d93e9ae1266e648c1e4e
Marvin van Aalst <marvin.vanaalst@gmail.com>
2026-05-28 10:09:12 +0200

    fix: url config

---
+++ src/lib/config.ts
@@ -0,0 +1,7 @@
+import { dev } from "$app/environment";
+
+export const title = "GreenSloth";
+export const description = "Green Sloth";
+export const url = dev
+  ? "http://localhost:5173/"
+  : "https://https://computational-biology-aachen.github.io/greensloth/";
